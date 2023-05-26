a2f791b5a62b1021fbdc5555cb41fa6ac4d1b17f
NekitPi <www.atom@list.ru>
2023-05-26 21:53:14 +0300

    отношения между жанром и книгами

--- books/models.py
+++ books/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50,blank=True)
+    author = models.CharField(max_length=50,blank=True)
     year = models.IntegerField()
+    raiting = models.IntegerField(default=0)
+
+    publisher = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return f"Книга: {self.id} Название: {self.title} Автор: {self.author}"
 
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True, related_name='books')
+
 class Genre(models.Model):
     title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Жанр: {self.id} Название: {self.title}"
