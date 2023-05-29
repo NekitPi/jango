a92f225b2d7e4957a77573de00eefc1296dd1bc5
NekitPi <www.atom@list.ru>
2023-05-29 20:09:47 +0300

    отношение категорий к фильмам

--- books/models.py
+++ books/models.py
@@ -5,7 +5,6 @@ class Book(models.Model):
     author = models.CharField(max_length=50,blank=True)
     year = models.IntegerField()
     raiting = models.IntegerField(default=0)
-
     publisher = models.CharField(max_length=50, null=True)
 
     def __str__(self):
@@ -18,3 +17,21 @@ class Genre(models.Model):
 
     def __str__(self):
         return f"Жанр: {self.id} Название: {self.title}"
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50,blank=True)
+    director = models.CharField(max_length=50,blank=True)
+    year = models.IntegerField()
+    raiting = models.IntegerField(default=0)
+    main_actor = models.CharField(max_length=50,blank=True)
+    category = models.ForeignKey("Category", on_delete=models.DO_NOTHING, null=True, blank=True, related_name='books')
+
+    def __str__(self):
+        return f"Фильм: {self.id} Название: {self.title} Автор: {self.director}"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Категория: {self.id} Название: {self.title}"
